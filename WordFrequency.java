dae4695dffea102202a0a6f0a4e98c0eb83caeea
avula-mounika <goudmounika13@gmail.com>
2021-09-14 20:48:13 +0530

    remove word

--- WordFrequency.java
+++ WordFrequency.java
@@ -6,25 +6,31 @@ import java.util.Map;
 public class WordFrequency {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String str = "To be or not to be ";
-		String [] strArray = str.split(" ");
-		Map<String, Integer> map =new HashMap<String, Integer>();
-		
-		for (String word : strArray) {
-			Integer number = map.get(word);
-			if(number==null) {
-				number=1;
-			}else {
-				number++;
-			}
-			map.put(word, number);
-		}
-		
-		for(Map.Entry<String, Integer>entry : map.entrySet()) {
-			System.out.println(entry.getKey() +"   "+entry.getValue());
-		}
+		 String str = "Paranoids are not paranoid because they are paranoid but " +
+	                "because they keep putting themselves deliberately " +
+	                "into paranoid avoidable situations";
 
-	}
+	        String [] strArray = str.split(" ");
+	        Map<String,Integer> map = new HashMap<String,Integer>();
+
+	        for (String word : strArray){
+	            word.hashCode();
+	            Integer number = map.get(word);
+	            if (number == null){
+	                number = 1;
+	            }
+	            else {
+	                number++;
+	            }
+	            map.put(word,number);
+	        }
+	        for (Map.Entry<String, Integer> entry : map.entrySet()){
+	            System.out.println(entry.getKey() + "  "+ entry.getValue());
+	            Integer returnValue = (Integer) map.remove("avoidable");
+
+				System.out.println(" Removing words value is: " + returnValue);
+				System.out.println(" After Removing :" + map);
+	        }
+	    }
 
 }
